e080f82daed6f6a15c10afc7d216f4c53ddceede
xavier dupré <xavier.dupre@gmail.com>
2019-01-14 00:23:17 +0100

    Update test_app.bat

--- bin/test_app.bat
+++ bin/test_app.bat
@@ -10,8 +10,9 @@ set pythonexe=c:\Python370_x64\python
 
 @echo ~SET pythonexe=%pythonexe%
 set pathserver=this,%~dp0..
-python -m mathenjeu static_local --cookie_key=dummypwd --start=1 --port=8877 --userpwd=abc --content=%pathserver%
 
+
+rem QCM
 
 rem python -m mathenjeu qcm_local --cookie_key=dummypwd --start=1 --port=8889 --userpwd=abc
 rem python -m mathenjeu create_self_signed_cert --keyfile=key.pem --certfile=cert.pem
@@ -20,3 +21,10 @@ rem python -m mathenjeu qcm_https --debug=1 --start=1 --port=8892 --userpwd=abc 
 rem daphne -e ssl:interface=127.0.0.1:port=8443:privateKey=key.pem:certKey=cert.pem apphyper:app
 rem hypercorn apphyper:app --keyfile=key.pem --certfile=cert.pem --bind=127.0.0.1:9443
 rem gunicorn apphyper:app --keyfile=key.pem --certfile=cert.pem --bind=127.0.0.1:9443
+
+rem LOCAL
+
+rem python -m mathenjeu static_local --cookie_key=dummypwd --start=1 --port=8877 --userpwd=abc --content=%pathserver%
+rem python -m mathenjeu create_self_signed_cert --keyfile=key.pem --certfile=cert.pem
+rem python -m mathenjeu static_https --debug=1 --start=1 --port=8892 --userpwd=abc --keyfile=key.pem --certfile=cert.pem --cookie_key=dummypwd --content=site,C:/site
+rem hypercorn apphyper:app --keyfile=key.pem --certfile=cert.pem --bind=127.0.0.1:9443
