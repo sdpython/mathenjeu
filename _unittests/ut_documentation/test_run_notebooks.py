56e87766167a6f3da8fdfb7d07fbea26869d065b
xavier dupré <xavier.dupre@gmail.com>
2019-01-13 02:29:13 +0100

    add new notebook

--- _unittests/ut_documentation/test_run_notebooks.py
+++ _unittests/ut_documentation/test_run_notebooks.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-@brief      test log(time=33s)
+@brief      test log(time=21s)
 """
 
 import sys
 import os
 import unittest
-import shutil
 from pyquickhelper.loghelper import fLOG
-from pyquickhelper.pycode import get_temp_folder
-from pyquickhelper.ipythonhelper import execute_notebook_list, execute_notebook_list_finalize_ut
+from pyquickhelper.ipythonhelper import test_notebook_execution_coverage
+import jyquickhelper
 
 
 try:
@@ -30,30 +29,34 @@ import src.mathenjeu
 
 class TestRunNotebooksPython(unittest.TestCase):
 
-    def test_run_notebook_python(self):
+    def setUp(self):
+        "module to import before executing a notebook"
+        self.assertTrue(jyquickhelper is not None)
+
+    def test_notebook_donnees_anonymisees(self):
         fLOG(
             __file__,
             self._testMethodName,
             OutputPrint=__name__ == "__main__")
 
-        temp = get_temp_folder(__file__, "temp_run_notebooks")
-        logs = os.path.join(temp, "logs")
-        os.mkdir(logs)
+        self.assertTrue(src.mathenjeu is not None)
+        folder = os.path.join(os.path.dirname(__file__),
+                              "..", "..", "_doc", "notebooks")
+        test_notebook_execution_coverage(__file__, "donnees_anonymisees", folder, 'mathenjeu',
+                                         copy_files=["logs/qcm100.txt"], fLOG=fLOG)
 
-        # selection of notebooks
-        fnb = os.path.normpath(os.path.join(
-            os.path.abspath(os.path.dirname(__file__)), "..", "..", "_doc", "notebooks"))
-        keepnote = []
-        for f in os.listdir(fnb):
-            if os.path.splitext(f)[-1] == ".ipynb" and "_long" not in f:
-                keepnote.append(os.path.join(fnb, f))
+    def test_notebook_example_logs(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
 
-        shutil.copy(os.path.join(fnb, "logs", "QCMApp.log"), logs)
-
-        # run the notebooks
-        res = execute_notebook_list(temp, keepnote, fLOG=fLOG,
-                                    additional_path=[os.path.dirname(src.__file__)])
-        execute_notebook_list_finalize_ut(res, fLOG=fLOG, dump=src.mathenjeu)
+        self.assertTrue(src.mathenjeu is not None)
+        self.assertTrue(jyquickhelper is not None)
+        folder = os.path.join(os.path.dirname(__file__),
+                              "..", "..", "_doc", "notebooks")
+        test_notebook_execution_coverage(__file__, "example_logs", folder, 'mathenjeu',
+                                         copy_files=["logs/QCMApp.log"], fLOG=fLOG)
 
 
 if __name__ == "__main__":
